57364dd4f50d20485f08fcc5254cdb359a9a829c
Dominique MICHEL <dominique.michel@epita.fr>
2020-09-03 03:53:36 +0200

    ADD new dependencies

--- src/NGN/__init__.py
+++ src/NGN/__init__.py
@@ -1,9 +1,11 @@
-import nntplib
 from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 from flask import Flask, request
+from apscheduler.schedulers.background import BackgroundScheduler
+import nntplib
+import atexit
 
 from chatbotmaker import Bot, Dispatcher, Database
 from chatbotmaker.database import add_relationship
@@ -13,4 +15,5 @@ from chatbotmaker.defaults.dev import DevMessenger
 from .models import database, Server, Channel
 from .dispatcher_cfg import dispatcher
 from .messenger import messenger, FACEBOOK_CHECK_TOKEN
+from .maintenance import check_subscription_updates
 from .app import app
